1850a10bc96c0b96cdd4daa325682d1abbc555a9
Garrett Myrick <garrettmyrick@gmail.com>
2018-07-16 20:32:32 -0700

    Switched back to using metaclasses. Subclassed JSONSchema to customize behavior.

--- sqlalchemy_jsonbase/jb.py
+++ sqlalchemy_jsonbase/jb.py
@@ -1,3 +1,4 @@
+import types
 import inspect
 import collections
 import sqlalchemy as sa
@@ -7,8 +8,36 @@ import datetime as dt
 
 from sqlalchemy.ext.declarative import declarative_base, DeclarativeMeta
 from sqlalchemy.ext.hybrid import hybrid_property
-from sqlalchemy_utils import get_class_by_table, get_declarative_base
+from sqlalchemy_utils import get_declarative_base
 from marshmallow_jsonschema import JSONSchema
+
+
+########################################################################################################################
+
+
+class _JSONSchema(JSONSchema):
+    """Customized behavior for Nested fields."""
+
+    def _get_schema_for_field(self, obj, field):
+        schema = super()._get_schema_for_field(obj, field)
+        fkey = getattr(field, 'foreign_class', None)
+        if fkey:
+            schema['idtype'] = fkey()
+        return schema
+
+    def _from_nested_schema(self, obj, field):
+        name = field.name
+        follow = obj.context.get('_follow', [])
+
+        if name in follow or name in self.context:
+            result = super()._from_nested_schema(obj, field)
+            result.pop('many', None)
+            return result
+
+        return {
+            '$ref': f'{field.related_class}#/definitions/{field.related_class}',
+            'type': 'object'
+        }
 
 
 ########################################################################################################################
@@ -57,21 +86,6 @@ class ViewSchema(mm.Schema):
 ########################################################################################################################
 
 
-class NestedSchemaPatch:
-    def __get__(self, instance, owner):
-        rel_schema = instance.related_class.__schema__
-        instance.nested = rel_schema
-        params = ViewSchema(context={'_exclude_rels': rel_schema}).dump({}).data
-        instance.exclude = params['exclude']
-        return instance.nested
-
-
-mm.fields.Nested.nested = NestedSchemaPatch()
-
-
-########################################################################################################################
-
-
 def column_default(col):
     if col.default:
         if col.default.is_callable:
@@ -87,9 +101,8 @@ def get_class_from_tablename(base, table):
     return None
 
 
-def _column_prop_to_field(cls, prop, name, opts):
+def _column_to_field(cls, col, name, opts):
     """Build a Field from a SQLAlchemy Column."""
-    col = prop.columns[0]
     info = col.info
 
     if 'field' in info:
@@ -109,21 +122,26 @@ def _column_prop_to_field(cls, prop, name, opts):
         'missing': column_default(col)
     }
 
+    options.update(opts)
+    options.update(info)
+
+    field = field_type(**options)
+
     foreign_keys = list(col.foreign_keys)
     if len(foreign_keys) == 1:
         fkey = foreign_keys[0]
         table_name = fkey.target_fullname.split('.')[0]
         decl_base = get_declarative_base(cls)
-        class_ = get_class_from_tablename(decl_base, table_name)
-        class_name = class_.__name__ if class_ else 'unknown'
-        options.update(foreign_key_class=class_name)
+
+        def foreign_class():
+            class_ = get_class_from_tablename(decl_base, table_name)
+            return class_.__name__ if class_ else 'unknown'
+
+        field.foreign_class = foreign_class
+
     elif len(foreign_keys) > 1:
         raise ValueError('Multiple foreign keys are not supported.')
 
-    options.update(opts)
-    options.update(info)
-
-    field = field_type(**options)
     return field
 
 
@@ -152,30 +170,11 @@ def _relationship_to_field(cls, rel, name, opts):
     if rel.uselist:
         options.update(many=True)
 
-    field = mm.fields.Nested(mm.Schema(), **options)
-    field.related_class = rel.argument()
-    del field.nested
+    rel_class = rel.argument().__name__
+    field = mm.fields.Nested(rel_class, **options)  # Schema has the same name as the class
+    field.related_class = rel_class
 
     return field
-
-
-def _instr_attr_to_field(cls, attr, name, opts):
-    field_type = FIELD_MAP.get(type(attr.prop), mm.fields.Raw)
-
-    if field_type is None:
-        return None
-
-    elif inspect.isclass(field_type) and issubclass(field_type, mm.fields.Field):
-        info = attr.info
-        options = dict(opts)
-        options.update(info)
-        return field_type(**options)
-
-    elif inspect.isfunction(field_type):
-        return field_type(cls, attr.prop, name, opts)
-
-    else:
-        raise TypeError(f'Unknown field type: {field_type}')
 
 
 ########################################################################################################################
@@ -191,8 +190,7 @@ FIELD_MAP = {
     bool: mm.fields.Boolean,
     dt.datetime: mm.fields.DateTime,
     dt.date: mm.fields.Date,
-    sa.orm.attributes.InstrumentedAttribute: _instr_attr_to_field,
-    sa.orm.ColumnProperty: _column_prop_to_field,
+    sa.Column: _column_to_field,
     hybrid_property: _hybrid_to_field,
     sa.orm.RelationshipProperty: _relationship_to_field
 }
@@ -232,6 +230,31 @@ def _make_field(cls, attr, name, opts):
 ########################################################################################################################
 
 
+class JsonMetaMixin:
+    """A mixin for the database model metaclass that automatically generates a marshmallow schema when the class is
+    created."""
+
+    def __init__(cls, name, bases, dict_):
+        super().__init__(name, bases, dict_)
+        schema_args = getattr(cls, '__schema_args__', {})
+
+        fields = {}
+        for attr_name, attr in dict_.items():
+            if attr_name.startswith('_'):
+                continue
+
+            field_args = schema_args.get(attr_name, {})
+            field = _make_field(cls, attr, attr_name, field_args)
+            if field:
+                fields[attr_name] = field
+
+        BaseSchema = getattr(cls, '__schema__', mm.Schema)
+        cls.__schema__ = type(f'{cls.__name__}', (BaseSchema,), fields)
+
+
+########################################################################################################################
+
+
 class JsonMixin:
     """Adds behaviors like serializing an object to JSON, updating from JSON, and getting schema information."""
 
@@ -246,8 +269,8 @@ class JsonMixin:
     def json_schema(cls, schema_attr='__schema__', **kwargs):
         """Return a JSON schema for the model's schema."""
         schema_cls = getattr(cls, schema_attr)
-        params = ViewSchema(context={'_exclude_rels': schema_cls}).dump(kwargs).data
-        return JSONSchema().dump(schema_cls(**params)).data
+        params = ViewSchema().dump(kwargs).data
+        return _JSONSchema().dump(schema_cls(**params)).data
 
     @classmethod
     def validate(cls, data, partial=False):
@@ -286,28 +309,8 @@ class JsonMixin:
 ########################################################################################################################
 
 
-JsonBase = declarative_base(cls=JsonMixin)
+class JsonBaseMeta(JsonMetaMixin, DeclarativeMeta):
+    pass
 
 
-def all_subclasses(cls):
-    return cls.__subclasses__() + [g for s in cls.__subclasses__() for g in all_subclasses(s)]
-
-
-@sa.event.listens_for(sa.orm.mapper, 'after_configured')
-def build_schemas():
-    models = all_subclasses(JsonMixin)
-    for model in models:
-        schema_args = getattr(model, '__schema_args__', {})
-
-        fields = {}
-        for attr_name, attr in model.__dict__.items():
-            if attr_name.startswith('_'):
-                continue
-
-            field_args = schema_args.get(attr_name, {})
-            field = _make_field(model, attr, attr_name, field_args)
-            if field:
-                fields[attr_name] = field
-
-        BaseSchema = getattr(model, '__schema__', mm.Schema)
-        model.__schema__ = type(f'{model.__name__}', (BaseSchema,), fields)
+JsonBase = declarative_base(cls=JsonMixin, metaclass=JsonBaseMeta)
